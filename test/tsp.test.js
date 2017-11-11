1965e51b60250fd8f333c427139cfed727805a78
Luis Crespo <lcrespom@gmail.com>
2017-11-12 00:12:42 +0100

    More tests

--- test/tsp.test.js
+++ test/tsp.test.js
@@ -23,9 +23,7 @@ describe('TspSolution', () => {
 	test('Each city appears only once', () => {
 		let map = new tsp.CountryMap(CITIES)
 		let sol = new tsp.TspSolution(map)
-		for (let i = 0; i < CITIES; i++) {
-			expect(arrayCount(sol.cities, i)).toBe(1)
-		}
+		expect(isPermutation(sol.cities)).toBe(true)
 	})
 	test('Invert works', () => {
 		let map = new tsp.CountryMap(CITIES)
@@ -34,6 +32,14 @@ describe('TspSolution', () => {
 		expect(sol.eval).toBe(inv.eval)
 		inv.cities.reverse()
 		expect(arraysEqual(sol, inv)).toBe(true)
+	})
+	test('Combine works', () => {
+		let map = new tsp.CountryMap(CITIES)
+		let sol1 = new tsp.TspSolution(map)
+		let sol2 = new tsp.TspSolution(map)
+		let lr = sol1.combine(sol2)
+		expect(isPermutation(lr[0].cities)).toBe(true)
+		expect(isPermutation(lr[1].cities)).toBe(true)
 	})
 })
 
@@ -47,5 +53,11 @@ function arraysEqual(a1, a2) {
 		return false
 	for (let i = 0; i < a1.length; i++)
 		if (a1[i] !== a2[i]) return false
+	return true
+}
+
+function isPermutation(a) {
+	for (let i = 0; i < a.length; i++)
+		if (arrayCount(a, i) != 1) return false
 	return true
 }
